1fed6d29affe08880c925700c569d9d158588024
Sean Cooper <seankamran@gmail.com>
2019-09-10 15:35:37 -0400

    5th Commit

--- src/App.js
+++ src/App.js
@@ -8,27 +8,36 @@ import Results from './Results';
 
 class App extends React.Component {
   state={
-    books: []
+    booksList:'',
+    currentVolume:'',
+
   }
 
-  updateState(response){
-      this.setState({volumeSearch:response})
+  updateStateVolumeList(response){
+      this.setState({booksList:response})
+  }
+
+  updateCurrentVolumeState(response){
+
   }
 
   getBooks = query => {
     console.log(query)
+    this.updateCurrentVolumeState(query);
     const baseURL = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
     console.log(baseURL)
       fetch(baseURL)
       .then(response => response.ok ? response.json() : Promise.reject("Something went wrong here"))
-      .then(response => this.updateState(response))
+      .then(response => this.updateStateVolumeList(response))
+      
   }
 
 
   render() {
     return (
       <div className="App">
-        <Search getBooks={this.getBooks} books={this.state.books}/>
+        <Search getBooks={this.getBooks} />
+        <Results books={this.state.booksList} />
       </div>
     )
   }
--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-class Results extends React.Component {
-
-    render(props){
-        const data=props.results;
-        return(
+class Results extends React.Component{
+  
+        render(){
+            console.log(this.props.books)
+            if(this.props.books !== 'null' || 'undefined') {
+                const data=this.props.books.items.map(newBook => (<li>{newBook.volumeInfo.title}</li>)) ;     
+            }
+        return (
             <ul class='resultsList'>
-                
+                {this.props.books}
             </ul>
         );
     }
