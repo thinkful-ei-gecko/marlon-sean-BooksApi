4de7ac603dfa24755b5cf55d7b046e310a2846a2
Marlon Agno <mdj.agno@gmail.com>
2019-09-10 16:54:58 -0400

    finished search

--- src/Results.js
+++ src/Results.js
@@ -5,11 +5,22 @@ class Results extends React.Component{
   
     render(){
         
-    const data=this.props.books.map(newBook => (<li>{newBook.volumeInfo.title}</li>)) ;    
+    const data=this.props.books.map(newBook => {
+        return(
+            <li key={newBook.id}>
+                <h3>{newBook.volumeInfo.title}</h3>
+                <img src={newBook.volumeInfo.imageLinks.thumbnail} alt={newBook.volumeInfo.title}></img>
+                <p>Author: {newBook.volumeInfo.authors}</p>
+                <p>{newBook.volumeInfo.description}</p>
+                {(newBook.saleInfo.saleability === 'FOR_SALE') ? <p>Price: {newBook.saleInfo.retailPrice.amount}</p> : <p></p>}
+            </li>
+            )
+        })
+     ;    
 
     return (
         <ul className='resultsList'>
-            {data}
+          {data}
         </ul>
     );
     }
